a31243375ae8420e815db676dc4efba72c97b32f
Pedro Vasconcelos <151564394+Pedro-H-Avila@users.noreply.github.com>
2024-09-16 22:36:10 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,3 +3,13 @@ public class Main {
         ReaderFile.processarArquivos();
     }
 }
+ /*
+// Lendo do Pendrive 
+public class Main {
+    public static void main(String[] args) {
+        // Exemplo: Caminho para o pendrive no Windows (ajuste conforme o sistema)
+        String pendrivePath = "E:/arquivos/";  // Caminho onde o pendrive está montado
+        ReaderFile.processarArquivos(pendrivePath);
+    }
+}
+*\
